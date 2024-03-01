3c8cdbd0615e032ef1020fb5415c62f11718b4e0
banoni-dev <banoni.bro@gmail.com>
2024-03-01 01:26:41 +0100

    some cleanup

--- 339-A.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    // 339-A (Helpful Maths)
-    let mut s = String::new();
-    std::io::stdin().read_line(&mut s).unwrap();
-    let mut s: Vec<&str> = s.trim().split('+').collect();
-    s.sort();
-    println!("{}", s.join("+"));
-}
