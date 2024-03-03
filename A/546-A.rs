402315ed693a3c0558f179a2fa73c08c9ba1be49
banoni-dev <banoni.bro@gmail.com>
2024-03-03 01:43:26 +0100

    resolving the 546-A problem

--- A/546-A.rs
+++ A/546-A.rs
@@ -1,11 +1,12 @@
 fn main() {
-    // 546-A
+
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
-    let mut input: Vec<i32> = input.split_whitespace().map(|x| x.parse().unwrap()).collect();
-    let mut n = input[0];
-    let mut k = input[1];
-    let mut w = input[2];
+    let mut input = input.split_whitespace();
+    let k: u32 = input.next().unwrap().parse().unwrap();
+    let n: u32 = input.next().unwrap().parse().unwrap();
+    let w: u32 = input.next().unwrap().parse().unwrap();
+
     let mut sum = 0;
     for i in 1..=w {
         sum += i * k;
