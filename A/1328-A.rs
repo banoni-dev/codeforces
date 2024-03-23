9f4e8a5340c80d2ea599726f249e8ced2af3b453
banoni-dev <banoni.bro@gmail.com>
2024-03-23 17:09:45 +0100

    correcting the implimetation of 705-A and 1328-A problems

--- A/1328-A.rs
+++ A/1328-A.rs
@@ -1,22 +1,20 @@
 fn main() {
-    // 1328-A divisibility problem
+    // 1328-A solution
     let mut t = String::new();
     std::io::stdin().read_line(&mut t).unwrap();
     let t: i32 = t.trim().parse().unwrap();
     for _ in 0..t {
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).unwrap();
-        let input: Vec<i32> = input
-            .split_whitespace()
-            .map(|x| x.parse().unwrap())
-            .collect();
-        let a = input[0];
-        let b = input[1];
+        let mut input = input.trim().split_whitespace();
+        let a: i32 = input.next().unwrap().parse().unwrap();
+        let b: i32 = input.next().unwrap().parse().unwrap();
+        let mut res = 0;
         if a % b == 0 {
             println!("0");
         } else {
-            println!("{}", b - a % b);
+            res = b - a % b;
+            println!("{}", res);
         }
     }
-
 }
--- A/705-A.rs
+++ A/705-A.rs
@@ -1,6 +1,8 @@
 fn main() {
-    // 705-A 
-    let n: i32 = read!();
+    // 705-A solution
+    let mut n = String::new();
+    std::io::stdin().read_line(&mut n).unwrap();
+    let n: i32 = n.trim().parse().unwrap();
     let mut s = String::new();
     for i in 1..n {
         if i % 2 == 0 {
@@ -15,4 +17,5 @@ fn main() {
         s.push_str("I hate it");
     }
     println!("{}", s);
+
 }
