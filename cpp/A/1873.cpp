d9c2e7902342c2162fc77410951a2841cad78a6f
banoni-dev <banoni.bro@gmail.com>
2024-05-09 23:53:47 +0100

    solving probelem 1873-A

---
+++ cpp/A/1873.cpp
@@ -0,0 +1,57 @@
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vll vector<long long>
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(c) (c).begin(), (c).end()
+#define sz(a) int((a).size())
+
+const int MOD = 1e9 + 7;
+const int INF = INT_MAX;
+const double PI = acos(-1.0);
+
+
+
+
+
+
+void solve() {
+    string s;    
+    cin >> s;
+    int res = (s[0] == 'a' || s[1] == 'b' || s[2] == 'c');
+    cout << (res ? "YES" : "NO") << endl;
+}
+
+
+
+
+
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
+
+
