02289cd6010a5cd93ed368ab3cdb4cdddbccc196
banoni-dev <banoni.bro@gmail.com>
2024-06-13 22:58:49 +0100

    solving the hey-A problem

---
+++ cpp/A/hey.cpp
@@ -0,0 +1,66 @@
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vll vector<long long>
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(c) (c).begin(), (c).end()
+#define sz(a) int((a).size())
+
+const int MOD = 1e9 + 7;
+const int INF = INT_MAX;
+const double PI = acos(-1.0);
+
+
+
+
+
+
+void solve() {
+   int n; 
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            cout << abs(a[i]) << " ";
+        } else {
+            cout << -abs(a[i]) << " ";
+        }
+    }
+}
+
+
+
+
+
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
+
+
